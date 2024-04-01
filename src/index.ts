54bc964999a6f1a6cf4aa5ee9885a500cc8af735
thong.duong@nois.vn <thong.duong@nois.vn>
2024-04-01 08:08:05 +0700

    process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const app = express();
 
 app.get("/", (req, res) => res.json({ answer: 43 }));
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("server running on port 4000");
 });
