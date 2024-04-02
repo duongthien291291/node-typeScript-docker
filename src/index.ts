70a46f13b295d0c773f352089c65eed280b32766
thong.duong@nois.vn <thong.duong@nois.vn>
2024-04-02 10:11:54 +0700

    force deploy

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const app = express();
 app.get("/", (req, res) => res.json({ answer: 43 }));
 
 app.listen(process.env.PORT || 80, () => {
-  console.log("server running on port 80");
+  console.log("server running on port 80!");
 });
